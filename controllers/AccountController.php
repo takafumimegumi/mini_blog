8201df7e2d985955c0b4041f56ca780d7bc0bef7
Takafumi Megumi <me@takafumimegumi.com>
2020-03-12 22:00:05 +0900

    signupビューファイルにエラー表示ブロックを追加し、inputのvalueに変数をセット

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -6,6 +6,8 @@ class AccountController extends Controller {
         // $_SESSION['csrf_tokens/account/signup']にトークンが格納される
         // renderメソッドを実行しているので、ビューファイル内で$_token変数に格納されたトークンが利用可
         return $this->render([
+            'user_name' => '',
+            'password' => '',
             '_token' => $this->generateCsrfToken('account/signup'),
         ]);
     }
--- views/account/signup.php
+++ views/account/signup.php
@@ -10,15 +10,23 @@ $this->setLayoutVar('title', 'アカウント登録');
     <!-- トークンをセットしCSRF対策 -->
     <input type="hidden" name="_token" value="<?= $this->escape($_token); ?>">
 
+    <?php if (isset($errors) && count($errors) > 0): ?>
+    <ul class="error_list">
+        <?php foreach($errors as $error): ?>
+        <li><?= $this->escape($error); ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+
     <table>
         <tbody>
             <tr>
                 <th>ユーザID</th>
-                <td><input type="text" name="user_name" value=""></td>
+                <td><input type="text" name="user_name" value="<?= $this->escape($user_name); ?>"></td>
             </tr>
             <tr>
                 <th>パスワード</th>
-                <td><input type="text" name="password" value=""></td>
+                <td><input type="text" name="password" value="<?= $this->escape($password); ?>"></td>
             </tr>
         </tbody>
     </table>
