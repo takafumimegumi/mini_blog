8d5ca943f1f1c446b01284fb1d67c7f7608554ab
Takafumi Megumi <me@takafumimegumi.com>
2020-03-12 22:34:12 +0900

    StatusRepositoryクラスを作成し、投稿処理を実装

---
+++ models/StatusRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+class StatusRepository extends DbRepository {
+
+    public function insert($user_id, $body) {
+        $now = new Datetime();
+
+        $sql = "insert into status (user_id, body, created_at) values (:user_id, :body, :created_at)";
+
+        $this->execute($sql, [
+            ':user_id' => $user_id,
+            ':body' => $body,
+            ':created_at' => $now->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    public function fetchAllPersonalArchivesByUserId($user_id) {
+        $sql = "
+            select a.*, u.user_name
+                from status as a
+            left join user as u
+                on a.user_id = u.id
+            where u.id = :user_id
+                order by a.created_at desc
+        ";
+
+        return $this->fetchAll($sql, [
+            ':user_id' => $user_id
+        ]);
+    }
+
+}
