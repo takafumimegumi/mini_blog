f3005df3fd5b7c50f1d542dbf9baa1c3227b381a
Takafumi Megumi <me@takafumimegumi.com>
2020-03-16 03:04:00 +0900

    FollowginRepositoryクラスを作成し、レコードを追加するメソッドと現在フォロー中かチェックするメソッドを定義

---
+++ models/FollowingRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+class FollowingRepository extends DbRepository {
+
+    public function insert($user_id, $following_id) {
+        $sql = "insert into following values(:user_id, :following_id)";
+
+        $stmt = $this->execute($sql, [
+            ':user_id' => $user_id,
+            ':following_id' => $following_id,
+        ]);
+    }
+
+    public function isFollowing($user_id, $following_id) {
+        $sql = "
+            select count(user_id) as count
+                from following
+            where user_id = :user_id
+                and following_id = :following_id
+        ";
+
+        $row = $this->fetch($sql, [
+            ':user_id' => $user_id,
+            ':following_id' => $following_id,
+        ]);
+
+        if ($row['count'] !== '0') {
+            return true;
+        }
+
+        return false;
+    }
+
+}
--- views/status/user.php
+++ views/status/user.php
@@ -6,6 +6,19 @@ $this->setLayoutVar('title', $user['user_name']);
 
 <h2><?= $this->escape($user['user_name']); ?></h2>
 
+<?php if (!is_null($following)): ?>
+<?php if ($following): ?>
+<p>フォローしています</p>
+<?php else: ?>
+<form action="<?= $base_url; ?>/follow" method="post">
+    <input type="hidden" name="_token" value="<?= $this->escape($_token) ?>">
+    <input type="hidden" name="following_name" value="<?= $this->escape($user['user_name']); ?>">
+    
+    <input type="submit" value="フォローする">
+</form>
+<?php endif; ?>
+<?php endif; ?>
+
 <div id="statuses">
     <?php foreach ($statuses as $status): ?>
     <?= $this->render('status/status', ['status' => $status]); ?>
